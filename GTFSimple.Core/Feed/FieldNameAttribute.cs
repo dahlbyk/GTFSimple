c12403062bdcbe07ed8f06987b6628ef48c1aa28
Keith Dahlby <dahlbyk@gmail.com>
2013-05-12 16:23:17 -0500

    Add Calendar mapping and tests

---
+++ GTFSimple.Core/Feed/FieldNameAttribute.cs
@@ -0,0 +1,15 @@
+using CsvHelper.Configuration;
+
+namespace GTFSimple.Core.Feed
+{
+    internal class FieldNameAttribute : CsvFieldAttribute
+    {
+        public FieldNameAttribute() { }
+
+        public FieldNameAttribute(string fieldName)
+        {
+            Name = fieldName;
+        }
+    }
+}
+

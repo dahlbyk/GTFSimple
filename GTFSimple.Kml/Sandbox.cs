32b556de70ec1f25841fc20f7e6e13483ef76e9b
Keith Dahlby <dahlbyk@gmail.com>
2013-09-02 21:35:18 -0500

    wip

--- GTFSimple.Kml/Sandbox.cs
+++ GTFSimple.Kml/Sandbox.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
 namespace GTFSimple.Kml
@@ -16,39 +13,38 @@ namespace GTFSimple.Kml
             if (root == null)
                 return;
 
-            var ns = (XNamespace) "http://www.opengis.net/kml/2.2";
+            var ns = (XNamespace)"http://www.opengis.net/kml/2.2";
             var document = from doc in root.Elements(ns + "Document")
-                           let docName = (string) doc.Element(ns + "name")
                            from folder in doc.Elements(ns + "Folder")
-                           let folderName = (string) folder.Element(ns + "name")
                            from placemark in folder.Elements(ns + "Placemark")
-                           let placemarkId = (string)placemark.Attribute("id")
-                           let placemarkName = (string)placemark.Element(ns + "name")
-                           let description = (string) placemark.Element(ns + "description")
-                           let routeMatch =
-                               Regex.Match(description, @"<td>Route</td>[^<]+<td>(?<route>[^<]+)</td>",
-                                           RegexOptions.Singleline).Groups["route"]
-                           let route = routeMatch == null ? "(none)" : routeMatch.Value
-                           group new { description, placemarkId, placemarkName, placemark } by route
-                           into g
-                           orderby g.Key
-                           select g;
+                           select new Placemark(placemark) into placemark
+                           group placemark by placemark.Route
+                               into g
+                               orderby g.Key
+                               select g;
 
             foreach (var g in document)
             {
                 Console.WriteLine(g.Key);
                 foreach (var s in g)
-                    Console.WriteLine("  " + s.placemarkId + ": " + s.placemarkName);
+                    Console.WriteLine("  " + s.Id + ": " + s.Name);
 
-                var kml = new XDocument(
-                    new XDeclaration("1.0", "UTF-8", "yes"),
-                    new XElement(ns + "Document"
-                        , new XElement(ns + "name", "Route " + g.Key)
-                                 , g.Select(x => x.placemark)
-                                 ));
-                kml.Save(FilePath(g.Key + ".kml"));
             }
 
+            var kml = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+                new XElement(
+                    ns + "Document"
+                    , new XElement(ns + "name", "Transit_Routes")
+                    , document.Select(g =>
+                                      new XElement(
+                                          ns + "Folder"
+                                          , new XElement(ns + "name", "Route " + g.Key)
+                                          , g.Select(x => (XElement)x)
+                                          ))));
+            var filePath = FilePath("ByRoute.kml");
+            Console.WriteLine(filePath);
+            kml.Save(filePath);
         }
 
         private static string FilePath(string kml)

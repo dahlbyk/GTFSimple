652e10653ab9504ead646598851d1273b3ebce75
Keith Dahlby <dahlbyk@gmail.com>
2013-10-14 22:30:56 -0500

    wip

--- GTFSimple.Web/Models/ShapesModel.cs
+++ GTFSimple.Web/Models/ShapesModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web.Mvc;
 using GTFSimple.Core.Feed;
 using GTFSimple.Kml;
+using Newtonsoft.Json;
+using SharpKml.Base;
 
 namespace GTFSimple.Web.Models
 {
@@ -40,32 +42,61 @@ namespace GTFSimple.Web.Models
             }
         }
 
+        public string GenerateSegmentsJson()
+        {
+            return JsonConvert.SerializeObject(GenerateSegments());
+        }
+
         public IEnumerable<Shape> GenerateShape()
+        {
+            var segments = GenerateSegments();
+
+            return segments.SelectMany(s => s.Coordinates)
+                           .DistinctUntilChanged()
+                           .Select((p, i) => new Shape
+                           {
+                               Id = ShapeId,
+                               PointLatitude = (decimal)p.Latitude,
+                               PointLongitude = (decimal)p.Longitude,
+                               PointSequence = (uint)i,
+                           });
+        }
+
+        private IEnumerable<Segment> GenerateSegments()
         {
             var route = repo.GetRoute(RouteId);
 
             var segments =
-                (
-                    from line in Segments.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
-                    let reverse = line[0] == '-'
-                    let id = line.Substring(1).Split(' ')[0]
-                    select new { id, reverse }
-                ).ToList();
+                from line in Segments.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+                let reverse = line[0] == '-'
+                let id = line.Substring(1).Split(' ')[0]
+                select new {id, reverse}
+                into x
+                let rs = route.GetSegment(x.id)
+                where rs != null
+                select new Segment(rs, x.reverse);
+            return segments;
+        }
+    }
 
-            var points = from x in segments
-                         let rs = route.GetSegment(x.id)
-                         where rs != null
-                         from c in x.reverse ? rs.Coordinates.Reverse() : rs.Coordinates
-                         select c;
+    public class Segment
+    {
+        private readonly RouteSegment rs;
+        private readonly bool reverse;
 
-            return points.DistinctUntilChanged()
-                         .Select((p, i) => new Shape
-                         {
-                             Id = ShapeId,
-                             PointLatitude = (decimal)p.Latitude,
-                             PointLongitude = (decimal)p.Longitude,
-                             PointSequence = (uint)i,
-                         });
+        public Segment(RouteSegment rs, bool reverse)
+        {
+            this.rs = rs;
+            this.reverse = reverse;
+        }
+
+        public string Id { get { return rs.Id; } }
+
+        public string Name { get { return rs.Name; } }
+
+        public IEnumerable<Vector> Coordinates
+        {
+            get { return reverse ? rs.Coordinates.Reverse() : rs.Coordinates; }
         }
     }
 }
